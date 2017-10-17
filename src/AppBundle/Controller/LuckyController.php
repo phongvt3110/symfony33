4664a02421ec3521328c4bd719a7f974aa24366e
phongvg3110 <vtphong651043@gmail.com>
2017-10-17 17:34:49 +0700

    update code

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -34,6 +34,51 @@ class LuckyController extends Controller
     public function welcomeAction(){
         return new Response('<h1>Welcome from the welcome Action</h1>');
     }
+
+    /**
+     * @Route("/lucky/welcome/{name}")
+     */
+    public function welcomesAction($name){
+        return new Response("<h1>Hello $name </h1>");
+    }
+
+    /**
+     * @Route("/page/{page}", name="showpage", requirements={"page": "\d+"})
+     */
+    public function showpageAction($page = 1){   //$page will have default value is 1
+        $data = ['mobile' => 'IphoneX',
+                 'page'  => $page
+                ];
+        return $this->render('frontend/pages.html.twig', [
+            'data' => isset($data)? $data: null,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/show/{_locale}/{year}/{slug}.{_format}",
+     *     defaults={"_format": "html"},
+     *     requirements={
+     *         "_locale": "en|fr",
+     *         "_format": "html|rss",
+     *         "year": "\d+"
+     *     }
+     * )
+     */
+    public function showAction($_locale, $year, $slug, $_format)
+    {
+        $data = ['mobile' => 'IphoneX',
+            'locale' => $_locale,
+            'year'  => $year,
+            'slug'  => $slug,
+            'format' => $_format
+        ];
+        return $this->render('frontend/show.html.twig', [
+            'data' => isset($data)? $data: null
+        ]);
+    }
+
     /**
      * @Route("/home")
      */
