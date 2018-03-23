51ee6c75898f840cf80ad5cb23164056e534b143
phongvg3110 <vtphong651043@gmail.com>
2018-03-23 17:07:09 +0700

    update code

--- src/AppBundle/Controller/backend/AdminController.php
+++ src/AppBundle/Controller/backend/AdminController.php
@@ -1,17 +1,23 @@
 <?php
-namespace AppBundle\Controller\squad;
+/**
+ * Created by PhpStorm.
+ * User: phongvt
+ * Date: 3/23/18
+ * Time: 4:55 PM
+ */
+namespace AppBundle\Controller\backend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class HomeController extends Controller
+class AdminController extends Controller
 {
     /**
-     * @Route("/", name="squadhomepage")
+     * @Route("/admin", name="adminhomepage")
      */
     public function indexAction() {
         $data = [];
-        return $this->render('Squad/index.html.twig.twig', [
+        return $this->render('AdminLTE/index.html.twig', [
             'data' => isset($data) ? $data : null
         ]);
     }
